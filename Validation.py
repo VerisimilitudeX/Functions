0ec7fe00f2851dea4fc8a9a84bfd21be92017769
Verisimilitude <acharyapiyush1@outlook.com>
2022-05-23 13:23:19 -0700

    Update Validation.py

--- Validation.py
+++ Validation.py
@@ -1,9 +1,3 @@
-"""
-LESSON: 6.1 - Functions
-TECHNIQUE 1: Validation
-DEMO
-"""
-
 #### ---- LIBRARIES --- ####
 import tsk
 import pygame
@@ -24,25 +18,20 @@ def draw_ships(num_ships):
         ship.draw()
         x += draw_increment
 
-
 #### ---------------------- ####
 #### ---- MAIN PROGRAM ---- ####
 #### ---------------------- ####
-# Setup
 w = pygame.display.set_mode([1018, 573])
 background = tsk.Sprite("AlienSpace.jpg", 0, 0)
 ships = int(input("How many ships should there be? "))
 
-# Main loop
 drawing = True
 while drawing:
 
-    # Event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             drawing = False
 
-    # Draw
     background.draw()
     draw_ships(ships)
     pygame.display.flip()
