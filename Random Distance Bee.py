ef5fd684128a189156e19f88f17332a0ec98a8fc
Verisimilitude <acharyapiyush1@outlook.com>
2022-05-23 13:16:31 -0700

    Update Random Distance Bee.py

--- Random Distance Bee.py
+++ Random Distance Bee.py
@@ -1,16 +1,8 @@
-"""
-LESSON: 6.1 - Functions
-WARMUP 1
-"""
-
 import random
 import tsk
 import pygame
 pygame.init()
 
-
-
-# Main program
 w = pygame.display.set_mode([1018, 573])
 c = pygame.time.Clock()
 
@@ -21,25 +13,20 @@ for i in range(10):
     bee = tsk.Sprite("Bee.png", 0, 50 + (i * 50))
     bees.append(bee)
 
-# Main loop
 drawing = True
 while drawing:
 
-    # Event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             drawing = False
 
-    # Move bees here
     for bee in bees:
         bee.x += random.randint(1, 20) * 0.025 * c.get_time()
 
-    # Draw
     background.draw()
     for bee in bees:
         bee.draw()
 
-    # Finish
     timer -= c.get_time()
     pygame.display.flip()
     c.tick(30)
@@ -47,7 +34,6 @@ while drawing:
     if timer <= 0:
         drawing = False
 
-# Find winning bee here
 max_x = 0
 for bee in bees:
     if bee.x > max_x:
