a84c92a8445976ed295d150b1bcf29c98c39c98e
Verisimilitude <acharyapiyush1@outlook.com>
2022-05-26 12:52:23 -0700

    Add files via upload

---
+++ Food on Counter.py
@@ -0,0 +1,51 @@
+"""
+LESSON: 6.1 - Functions
+WARMUP 5
+"""
+
+# Libraries
+import random
+import tsk
+import pygame
+pygame.init()
+
+#### ---------------------------- ####
+#### ---- DRAW FOOD FUNCTION ---- ####
+#### ---------------------------- ####
+def draw_food(number):
+    if number > 5 or number < 1:
+        return
+    files = ["BurgerPatty.png", "CheeseSlice.png", "HamSlice.png", "LettuceLeaf.png", "TomatoSlice.png"]
+    image  = files[number - 1]
+    food = tsk.Sprite(image, random.randint(0, 750), random.randint(400, 500))
+    food.draw()
+
+#### ---------------------- ####
+#### ---- MAIN PROGRAM ---- ####
+#### ---------------------- ####
+
+w = pygame.display.set_mode([1018, 573])
+background = tsk.Sprite("Restaurant.jpg", 0, 0)
+background.draw()
+pygame.display.flip()
+
+#### ---- MAIN LOOP ---- ####
+drawing = True
+while drawing:
+
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            drawing = False
+
+    # Get input
+    value = int(input("Enter a number, 1 - 5 or 0 to quit "))
+
+    # Call function here
+    draw_food(value)
+
+    # Finish
+    pygame.display.flip()
+    if value == 0:
+        drawing = False
+
