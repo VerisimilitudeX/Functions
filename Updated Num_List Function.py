2699786c37914f5b6957b717d2b8afb42cffb344
Verisimilitude <acharyapiyush1@outlook.com>
2022-05-27 13:02:51 -0700

    Update Updated Num_List Function.py

--- Updated Num_List Function.py
+++ Updated Num_List Function.py
@@ -7,6 +7,8 @@ WARMUP 1
 #### ---- SUM LIST FUNCTION ---- ####
 #### --------------------------- ####
 def sum_list(num_list):
+    if len(num_list) == 0:
+        return 0
     total = 0
     for num in num_list:
         total += num
